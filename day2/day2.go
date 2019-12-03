78b81b6aff2548ef523db67aa4700dfa6cb63be3
Joseph Bauer <root@thesudorm.com>
2019-12-02 20:11:36 -0500

    Stuck on part 2 of day 2...

--- day2/day2.go
+++ day2/day2.go
@@ -17,16 +17,6 @@ func main(){
 		log.Fatal(err);
 	}
 
-	/*
-	contents, err := ioutil.ReadAll(file)
-	if err != nil {
-		log.Fatal(err);
-	}
-
-	fmt.Println(contents)
-	fmt.Println()
-	*/
-
 	input, err := csv.NewReader(file).ReadAll()
 
 	fmt.Println(input)
@@ -41,23 +31,38 @@ func main(){
 		data = append(data, value)
 	}
 
-	for i := 0; i < len(data) - 3; i += 4 {
-		firstValue := data[data[i + 1]]
-		secondValue := data[data[i + 2]]
-		position := data[i + 3]
+	originalData := data
 
-		if data[i] == 1 { // Add
-			data[position] = firstValue + secondValue
-		} else if data[i] == 2 { // Multiply
-			data[position] = firstValue * secondValue
-		} else if data[i] == 99 {
-			fmt.Println("Done")
-			break
-		} else {
-			fmt.Println(data[i])
-			log.Fatal("Something went wrong")
+	for j := 0; j < 100; j++ {
+		for k := 0; k < 100; k++ {
+			data = originalData
+			data[1] = int64(j)
+			data[2] = int64(k)
+
+			for i := 0; i < len(data) - 3; i += 4 {
+				firstValue := data[data[i + 1]]
+				secondValue := data[data[i + 2]]
+				position := data[i + 3]
+
+				if data[i] == 1 { // Add
+					data[position] = firstValue + secondValue
+				} else if data[i] == 2 { // Multiply
+					data[position] = firstValue * secondValue
+				} else if data[i] == 99 {
+					break
+				} else {
+					break;
+				}
+			}
+
+			fmt.Println("Noun: ", j, "Verb: ", k, "Answer: ", data[0])
+			fmt.Println(data)
+
+			if data[0] == 19690720 {
+				fmt.Println(j," ", k)
+				break;
+			}
+
 		}
 	}
-
-	fmt.Println(data)
 }
