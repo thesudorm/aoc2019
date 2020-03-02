ee45d41e101c2d251f628bd3f841fa7810c357a7
Joseph Bauer <root@thesudorm.com>
2020-03-02 11:57:28 -0500

    Day one and two in better format..

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -5,22 +5,40 @@ fn main() {
     let filename = "src/day1.txt";
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
-    let mut sum = 0;
+    let mut total1 = 0;
+    let mut total2 = 0;
 
     for line in reader.lines() {
         let line = line.expect("Failed to read line");
-        let mut fuel: u32 = line.trim().parse()
+        let mass: usize = line.trim().parse()
             .expect("Didn't read a number");
 
-        loop {
-            if fuel >= 6 {
-                fuel = (fuel / 3) - 2;
-                sum += fuel;
-            } else {
-                break;
-            }
-        }
+        total1 += part_one(mass);
+        total2 += part_two(mass);
     }
 
-    println!("Sum: {}", sum);
+    println!("Total 1: {}   Total 2: {}", total1, total2);
+}
+
+fn part_one(x: usize) -> usize {
+    match (x / 3).checked_sub(2) {
+        Some(y) => y,
+        None => 0,
+    }
+}
+
+fn part_two(x: usize) -> usize {
+    let mut total = 0;
+    let mut x = x;
+    loop {
+        let y = match (x / 3).checked_sub(2) {
+            Some(y) => y,
+            None => {
+                break total;
+            }
+        };
+        println!("{}", y);
+        x = y;
+        total += y;
+    }
 }
