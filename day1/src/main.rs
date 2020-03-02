b53efb29b97ac3a8413d3132b918679773331fc9
Joseph Bauer <root@thesudorm.com>
2020-03-02 09:18:42 -0500

    Fished day 1, part 1.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,11 +2,17 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-    let filename = "day1.txt";
+    let filename = "src/day1.txt";
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
+    let mut sum = 0;
 
     for line in reader.lines() {
-        println!("{}", line);
+        let line = line.expect("Failed to read line");
+        let line: u32 = line.trim().parse()
+            .expect("Didn't read a number");
+        sum += (line / 3) - 2;
     }
+
+    println!("Sum: {}", sum);
 }
